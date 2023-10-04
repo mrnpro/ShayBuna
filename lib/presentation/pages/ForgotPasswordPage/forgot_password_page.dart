094aff633c169417f917fb83ebd9086d567b00a7
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-10-04 06:15:25 +0300

    Create forgot_password_page.dart

---
+++ lib/presentation/pages/ForgotPasswordPage/forgot_password_page.dart
@@ -0,0 +1,172 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../core/constants/assets.dart';
+import '../../../core/constants/colors.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final _emailController = TextEditingController();
+  final TextStyle poppinsStyle = GoogleFonts.poppins(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: MyColors.kSecondaryColor);
+  final TextStyle mochiyPopOneStyle = GoogleFonts.mochiyPopOne(
+      fontWeight: FontWeight.bold,
+      fontSize: 25.sp,
+      color: MyColors.kPrimaryColor);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: MyColors.kPrimaryColor, body: _body(context));
+  }
+
+  late FocusNode _emailFocus;
+  late bool _emailHasFocus;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailFocus = FocusNode();
+    _emailHasFocus = false;
+
+    // listeners
+    _emailFocus.addListener(_onEmailFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _emailFocus.removeListener(_onEmailFocusChange);
+    _emailFocus.dispose();
+    _emailController.clear();
+
+    super.dispose();
+  }
+
+  void _onEmailFocusChange() {
+    setState(() {
+      _emailHasFocus = _emailFocus.hasFocus;
+    });
+  }
+
+  _body(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: SafeArea(
+          child: SizedBox(
+              width: size.width,
+              child: SingleChildScrollView(
+                  child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(children: [
+                  _arrowBack(context),
+                  _shaybunaLogo(),
+                  _emailAdressField(size),
+                  _sendResetCodeBtn(size),
+                ]),
+              )))),
+    );
+  }
+
+  _arrowBack(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width,
+      child: Row(
+        children: [
+          Align(
+            alignment: Alignment.centerLeft,
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: const Icon(
+                Icons.arrow_back,
+                size: 39,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _sendResetCodeBtn(Size size) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: 50,
+        width: size.width,
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+              backgroundColor: MyColors.kSecondaryColor, elevation: 3),
+          onPressed: _onLoginBtnPressed,
+          child: Text(
+            "Reset",
+            style: mochiyPopOneStyle.copyWith(
+                color: MyColors.kPrimaryColor, fontSize: 15),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Column _shaybunaLogo() {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 10,
+        ),
+        Hero(tag: 'shay_buna_logo', child: SvgPicture.asset(Assets.assetsLogo)),
+      ],
+    );
+  }
+
+  Column _emailAdressField(Size size) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 50,
+        ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          width: size.width,
+          height: 51,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(14),
+              border: Border.all(
+                  color: MyColors.kSecondaryColor,
+                  width: _emailHasFocus ? 1.2 : 0),
+              color: const Color(0xffdacabd)),
+          child: Row(children: [
+            const Icon(
+              Icons.email,
+              color: MyColors.kSecondaryColor,
+            ),
+            Expanded(
+                child: TextFormField(
+              focusNode: _emailFocus,
+              controller: _emailController,
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                  border: InputBorder.none, hintText: "Your Email Adress"),
+            ))
+          ]),
+        )
+      ],
+    );
+  }
+
+  void _onLoginBtnPressed() {}
+}
