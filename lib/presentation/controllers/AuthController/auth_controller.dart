9c18651a983624c1679406251d5fea16982dd916
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-10-04 21:02:37 +0300

    Create auth_controller.dart

---
+++ lib/presentation/controllers/AuthController/auth_controller.dart
@@ -0,0 +1,44 @@
+import 'package:coffee_shop/data/AuthData/auth_data.dart';
+import 'package:coffee_shop/presentation/controllers/AuthController/auth_state.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final authNotifierProvider =
+    StateNotifierProvider<AuthController, AuthenticationState>(
+        (ref) => AuthController(authData: ref.read(authDataProvider)));
+
+class AuthController extends StateNotifier<AuthenticationState> {
+  final AuthData _authData;
+
+  AuthController({required AuthData authData})
+      : _authData = authData,
+        super(const AuthenticationState.initial());
+
+  Future<void> login({required String email, required String password}) async {
+    state = const AuthenticationState.loading();
+    final response = await _authData.signIn(email: email, password: password);
+    state = response.fold((l) => AuthenticationState.error(error: l),
+        (r) => AuthenticationState.authenticated(user: r));
+  }
+
+  Future<void> signUp(
+      {required String email,
+      required String password,
+      required String phoneNumber}) async {
+    state = const AuthenticationState.loading();
+    final response = await _authData.signUp(
+        email: email, password: password, phoneNumber: phoneNumber);
+    state = response.fold((l) => AuthenticationState.error(error: l),
+        (r) => AuthenticationState.authenticated(user: r));
+  }
+
+  Future<void> signOut() async {
+    await _authData.signOut();
+  }
+
+  Future<void> forgotPassword(String email) async {
+    state = const AuthenticationState.loading();
+    final response = await _authData.forgotPassword(email: email);
+    state = response.fold((l) => AuthenticationState.error(error: l),
+        (r) => AuthenticationState.wildSucceed(message: r));
+  }
+}
