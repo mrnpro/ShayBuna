10ebe14a501bc1a5bcd3ca4d51cce10f06e2c365
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2023-10-04 21:00:24 +0300

    reusable text style

---
+++ lib/core/constants/text_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'colors.dart';
+
+final TextStyle poppinsStyle = GoogleFonts.poppins(
+    fontSize: 14, fontWeight: FontWeight.w400, color: MyColors.kSecondaryColor);
+final TextStyle mochiyPopOneStyle = GoogleFonts.mochiyPopOne(
+    fontWeight: FontWeight.bold,
+    fontSize: 26.sp,
+    color: MyColors.kPrimaryColor);
