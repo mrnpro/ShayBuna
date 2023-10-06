00249edeec403f3d570d51886857d17ccba29f6a
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-07 02:54:45 +0300

    Create coffee_model.dart
    used freezed 😉 for code generation

---
+++ lib/data/Models/coffee_model.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+part 'coffee_model.freezed.dart';
+part 'coffee_model.g.dart';
+
+@freezed
+class CoffeeModel with _$CoffeeModel {
+  const factory CoffeeModel({
+    required String title,
+    required String price,
+    required String category,
+    required String description,
+    required String imagePath,
+    required bool hasMilk,
+  }) = _CoffeeModel;
+  factory CoffeeModel.fromJson(Map<String, dynamic> json) =>
+      _$CoffeeModelFromJson(json);
+}
