76918f822c9f6ebfa213d97d1a8f8caa1608e5c3
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-07 02:55:43 +0300

    Create coffee_category_controller.dart

---
+++ lib/presentation/controllers/CoffeeController/coffee_category_controller.dart
@@ -0,0 +1,22 @@
+import 'package:coffee_shop/data/CoffeeData/coffee_data_impl.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final cofeeCategoryNotifier =
+    StateNotifierProvider<CofeeCategoryController, AsyncValue<List<String>>>(
+        (ref) => CofeeCategoryController(ref: ref)..fetchAllCategories());
+final categoryIndexStateProvider = StateProvider<int>((ref) => 0);
+
+class CofeeCategoryController extends StateNotifier<AsyncValue<List<String>>> {
+  final Ref _ref;
+  int selectedCategoryIndex = 0;
+  CofeeCategoryController({required Ref ref})
+      : _ref = ref,
+        super(const AsyncValue.loading());
+
+  void fetchAllCategories() async {
+    final response = await _ref.read(coffeeDataProvider).getCategories();
+    state = response.fold(
+        (error) => AsyncValue.error(error, StackTrace.current),
+        (data) => AsyncValue.data(["All", ...data]));
+  }
+}
