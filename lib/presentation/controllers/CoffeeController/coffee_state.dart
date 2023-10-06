f9a9adcadfc90ed0f657e92725142807a31ec233
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-07 02:55:59 +0300

    Create coffee_state.dart

---
+++ lib/presentation/controllers/CoffeeController/coffee_state.dart
@@ -0,0 +1,12 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import '../../../data/Models/coffee_model.dart';
+part 'coffee_state.freezed.dart';
+
+@freezed
+abstract class CoffeeState with _$CoffeeState {
+  factory CoffeeState.initial() = _Initial;
+  factory CoffeeState.loading() = _Loading;
+  factory CoffeeState.loaded({required List<CoffeeModel> coffees}) = _Loaded;
+  factory CoffeeState.error(String error) = _Error;
+}
