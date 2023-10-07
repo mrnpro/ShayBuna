79bc20d4582e3b609c0542da6d207a7ff3134845
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-07 19:18:59 +0300

    remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'package:coffee_shop/firebase_options.dart';
-import 'package:coffee_shop/presentation/pages/HomePage/home_page.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import 'core/theme/my_theme.dart';
-import 'data/CoffeeData/mock_data.dart';
 import 'presentation/pages/onboardingPage/onboarding_page.dart';
-import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
