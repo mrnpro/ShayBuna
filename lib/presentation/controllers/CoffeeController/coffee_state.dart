6c932381fb44caa17853ae8dac8dde82377174d0
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-07 19:22:03 +0300

    Update coffee_state.dart

--- lib/presentation/controllers/CoffeeController/coffee_state.dart
+++ lib/presentation/controllers/CoffeeController/coffee_state.dart
@@ -1,6 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import '../../../data/Models/coffee_model.dart';
+import '../../../Models/coffee_model.dart';
 part 'coffee_state.freezed.dart';
 
 @freezed
