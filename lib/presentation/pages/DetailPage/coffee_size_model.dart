62410a932b5a4d6071632660b26f7d6e7ada4f19
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-10 01:01:52 +0300

    Create coffee_size_model.dart

---
+++ lib/presentation/pages/DetailPage/coffee_size_model.dart
@@ -0,0 +1,11 @@
+class CoffeeSizeModel {
+  final String size;
+
+  CoffeeSizeModel({required this.size});
+}
+
+final List<CoffeeSizeModel> coffeeSizeList = [
+  CoffeeSizeModel(size: "L"),
+  CoffeeSizeModel(size: "M"),
+  CoffeeSizeModel(size: "S")
+];
