1b870101a3b9d00e718144d5649ebbd6a5618be2
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-10 01:13:10 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
+import 'package:chapa_unofficial/chapa_unofficial.dart';
 import 'package:coffee_shop/firebase_options.dart';
+import 'package:coffee_shop/presentation/pages/HomePage/home_page.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+import 'core/config/config.dart';
 import 'core/theme/my_theme.dart';
+import 'presentation/bottom_nav.dart';
 import 'presentation/pages/onboardingPage/onboarding_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // initialize firebase
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  // initialize app check
+  await FirebaseAppCheck.instance.activate();
+  // load env config file
+  await dotenv.load(fileName: ".env");
+  // configure chapa
+  Chapa.configure(privateKey: Config.chapaSecretKey);
   runApp(const ProviderScope(child: MyApp()));
 }
 
