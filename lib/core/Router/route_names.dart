54ddd750690e9a28ec9033282a78ef1cf635fef1
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-11 22:45:47 +0300

    Create route_names.dart

---
+++ lib/core/Router/route_names.dart
@@ -0,0 +1,13 @@
+class RouteNames {
+  static get loginPath => "/login";
+  static get registerPath => '/register';
+  static get forgotPasswordPath => '/forgot_password';
+  static get coffeeDetailPath => '/coffee_detail';
+  static get botomNavPath => '/bottom_nav';
+  static get onboardingPath => '/onboarding';
+  static get ordersPath => '/orders';
+  static get coffeeSearchPath => '/coffee_search';
+  static get paymentSuccessPath => '/payment_success';
+  static get paymentErrorPath => '/payment_error';
+  static get splashPath => '/splash';
+}
