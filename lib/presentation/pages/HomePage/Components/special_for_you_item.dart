42a19900be5b3795358481755791bd291060a053
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-11 22:51:21 +0300

    Update special_for_you_item.dart

--- lib/presentation/pages/HomePage/Components/special_for_you_item.dart
+++ lib/presentation/pages/HomePage/Components/special_for_you_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
+import '../../../../Models/coffee_model.dart';
 import '../../../../core/constants/assets.dart';
 import '../../../../core/constants/colors.dart';
 import '../../../../core/constants/common_widgets.dart';
@@ -9,58 +11,64 @@ class SpecialForYouItem extends StatelessWidget {
     super.key,
     required this.size,
     required this.poppinsStyle,
+    required this.coffeeModel,
+    required this.onPressed,
   });
 
   final Size size;
   final TextStyle poppinsStyle;
-
+  final CoffeeModel coffeeModel;
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        width: size.width,
-        height: 113,
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(29),
-            boxShadow: [kBoxShadow],
-            color: MyColors.kSecondaryColor),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text("Cappuccino",
-                      style: poppinsStyle.copyWith(
-                          fontSize: 24,
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          width: size.width,
+          height: 113,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(29),
+              boxShadow: [kBoxShadow],
+              color: MyColors.kSecondaryColor),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(coffeeModel.title,
+                        style: poppinsStyle.copyWith(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w700,
+                            color: MyColors.kPrimaryColor)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: Text(coffeeModel.discountText ?? "No Discount",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: MyColors.kPrimaryColor,
                           fontWeight: FontWeight.w700,
-                          color: MyColors.kPrimaryColor)),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: Text("10 % Discount",
-                      style: TextStyle(
-                        fontSize: 16,
-                        color: MyColors.kPrimaryColor,
-                        fontWeight: FontWeight.w700,
-                      )),
-                )
-              ],
-            ),
-            Container(
-              margin: const EdgeInsets.all(10),
-              width: 103,
-              height: 113,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(25),
-                  border: Border.all(color: MyColors.kPrimaryColor, width: 3),
-                  image: const DecorationImage(
-                      image: AssetImage(Assets.assetsCapuccinoImage))),
-            )
-          ],
-        ));
+                        )),
+                  )
+                ],
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                width: 103,
+                height: 113,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    border: Border.all(color: MyColors.kPrimaryColor, width: 3),
+                    image: const DecorationImage(
+                        image: AssetImage(Assets.assetsCapuccinoImage))),
+              )
+            ],
+          )),
+    );
   }
 }
