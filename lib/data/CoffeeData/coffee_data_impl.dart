b2a387a1b3824be27a3a3a89d08c6d7918188b11
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-11 22:47:39 +0300

    refactor

--- lib/data/CoffeeData/coffee_data_impl.dart
+++ lib/data/CoffeeData/coffee_data_impl.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 
-import 'package:coffee_shop/core/providers/general_providers.dart';
+import 'package:coffee_shop/riverpod_container.dart';
 import 'package:coffee_shop/data/CoffeeData/coffee_data.dart';
 import 'package:dartz/dartz.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -19,17 +19,7 @@ class CoffeeDataImpl implements CoffeeData {
   @override
   Future<Either<String, List<CoffeeModel>>> getCoffees() async {
     try {
-      final reference = _ref.read(firebaseDatabaseProvider).ref();
-
-      final firebaseCoffeeDatas = await reference.child('coffee_data').get();
-      final coffeeDatas = <CoffeeModel>[];
-
-      final encodedValue = jsonEncode(firebaseCoffeeDatas.value);
-
-      for (var element in jsonDecode(encodedValue)) {
-        coffeeDatas.add(CoffeeModel.fromJson(element));
-      }
-
+      final coffeeDatas = await _getCoffees('coffee_data');
       return Right(coffeeDatas);
     } on FirebaseException catch (e) {
       return left(e.message ?? "something went wrong please try again latter");
@@ -52,6 +42,37 @@ class CoffeeDataImpl implements CoffeeData {
       return Right(categories);
     } catch (e) {
       return left(e.toString());
+    }
+  }
+
+  @override
+  Future<Either<String, List<CoffeeModel>>> getSpecialData() async {
+    final userId = _ref.read(firebaseAuthProvider).currentUser?.uid;
+    if (userId == null) {
+      return const Left("UnAuthorized");
+    }
+    final coffeeDatas = await _getCoffees('special_coffee_data');
+    try {
+      return Right(coffeeDatas);
+    } on FirebaseException catch (e) {
+      return Left(e.message ?? "something went wrong , please try again later");
+    }
+  }
+
+  Future<List<CoffeeModel>> _getCoffees(String path) async {
+    try {
+      final reference = _ref.read(firebaseDatabaseProvider).ref(path);
+      final firebaseData = await reference.get();
+      final coffeeDatas = <CoffeeModel>[];
+
+      final encodedValue = jsonEncode(firebaseData.value);
+
+      for (var element in jsonDecode(encodedValue)) {
+        coffeeDatas.add(CoffeeModel.fromJson(element));
+      }
+      return coffeeDatas;
+    } catch (e) {
+      rethrow;
     }
   }
 }
