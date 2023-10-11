c071b8384dd50cd76a160de62baa276f56336f1e
Abraham Ayana <88460514+mrnpro@users.noreply.github.com>
2023-10-11 22:45:02 +0300

    Create firebase_db_references.dart
    separate db references in one file

---
+++ lib/core/constants/firebase_db_references.dart
@@ -0,0 +1,5 @@
+const customersRef = 'customers';
+const coffeeDataRef = 'coffee_data';
+const categoriesRef = 'categories';
+const ordersRef = 'orders';
+const specialCoffeeDataRef = 'special_coffee_data';
