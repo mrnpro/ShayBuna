e9f357e90fde1b760bba35a9e212d8623e61120d
Abraham Ayana <yegetanaty03@gmail.com>
2023-10-21 17:27:42 +0300

    add new color

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../utils/color_util.dart';
 
 final class MyColors {
+  static const kGrey = Color(0xffdacabd);
   static final kPrimaryColor = HexColor('EBDBCC');
   static const kSecondaryColor = Color(0xff301001);
   static final kThirdColor = HexColor('FFB594');
